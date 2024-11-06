a9ec8e567bd14ee7b6795f1f1550c6ccbcd4a86f
Rashicom <rashid.kp484@gmail.com>
2024-11-06 21:10:53 +0530

    page render

--- AlRaihan/products/models.py
+++ AlRaihan/products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="catagory")
     def __str__(self):
         return str(f"Category: {self.name}")
     
