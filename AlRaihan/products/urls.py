6dbfdbf4d20f5b87a4f16310e212e9ce03ecf6a1
Rashicom <rashid.kp484@gmail.com>
2024-11-07 22:00:53 +0530

    html update

--- AlRaihan/products/urls.py
+++ AlRaihan/products/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("shope/", Shop.as_view(), name="shop"),
     path("contact/", Contact.as_view(), name="contact"),
+    
     path("cart/", Cart.as_view(), name="cart"),
     path("checkout/", Checkout.as_view(), name="checkout"),
     path("thanks/", Thank.as_view(), name="thanks"),
