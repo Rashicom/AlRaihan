21dfc4a92692d4ddcc5f9ab1c80b325760cb36d0
zayanft <anshidp2e@gmail.com>
2024-11-23 09:44:12 +0530

    change commit message

--- AlRaihan/config/settings.py
+++ AlRaihan/config/settings.py
@@ -118,11 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Correct
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Separate path
 
 
 MEDIA_URL = 'media/'
